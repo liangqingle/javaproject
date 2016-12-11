5c14d3b9dcdf58d2600a0e948b73a5e0a81582a4
liangqingle <liangqingle@192.168.1.5>
2016-12-11 10:01:45 +0800

    提交者：liangqingle <liangqingle@192.168.1.5>
     位于分支 master
    
     初始提交
    
     要提交的变更：
    	新文件：   liangqingle/helloworld/.pom.xml.swp
            新文件：   liangqingle/helloworld/pom.xml
    	新文件：   liangqingle/helloworld/src/main/java/com/liangqingle/study/helloworld/.HelloWorld.java.swp
    	新文件：   liangqingle/helloworld/src/main/java/com/liangqingle/study/helloworld/HelloWorld.java
    	新文件：   liangqingle/helloworld/src/test/java/com/liangqingle/study/helloworld/.HelloWorldTest.java.swp
    	新文件：   liangqingle/helloworld/src/test/java/com/liangqingle/study/helloworld/HelloWorldTest.java
    	新文件：   liangqingle/helloworld/target/classes/com/loangqingle/study/helloworld/HelloWorld.class
    	新文件：   liangqingle/helloworld/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    	新文件：   liangqingle/helloworld/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst
    	新文件：   liangqingle/helloworld/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/createdFiles.lst
    	新文件：   liangqingle/helloworld/target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst
    	新文件：   liangqingle/helloworld/target/surefire-reports/TEST-com.liangqingle.study.helloworld.HelloWorldTest.xml
    	新文件：   liangqingle/helloworld/target/surefire-reports/com.liangqingle.study.helloworld.HelloWorldTest.txt
    	新文件：   liangqingle/helloworld/target/test-classes/com/liangqingle/study/helloworld/HelloWorldTest.class

---
+++ liangqingle/helloworld/src/main/java/com/liangqingle/study/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.loangqingle.study.helloworld;
+
+public class  HelloWorld{
+    public static void main(String[] args){
+        System.out.println("hi world");
+    }
+}
+                             
---
+++ liangqingle/helloworld/src/test/java/com/liangqingle/study/helloworld/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.liangqingle.study.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorldTest{
+    @Test
+    public void testSayHello(){
+    }
+}
